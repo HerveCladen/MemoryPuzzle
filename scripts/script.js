96c35828e6dff58142cba7e202d7a5d08c085564
Hervé Claden <hervecladen@gmail.com>
2019-04-03 19:42:25 -0400

    Logic added
    Game works as it should (with some issues)
    Can generate grids from 2x2 to 8x8 (with an input)
    Each pair of cards has its own color

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
-window.onload = generation(6,8);
+window.onload = generation(4,5);
+
+var previouslyFlipped, counter = 0;
 
 function generation(rowsInsert, columnsInsert) {
+    counter = 0;
     let board = document.getElementById("board");
+    board.innerHTML = "";
     for(i = 0; i < rowsInsert; i++) {
         board.innerHTML += '<div class="row"></div>';
     }
@@ -17,19 +21,40 @@ function generation(rowsInsert, columnsInsert) {
     for(i = 0; i < cards.length; i++) {
         cardNumbers.push(i+1);
     } 
-
     cardNumbers = shuffle(cardNumbers);
-    console.log(cards);
-    console.log(cardNumbers);
 
-    var pairs = cards.length / 2;
+    var pairs = cards.length / 2, number;
     for(i = 0; i < cards.length; i++) {
-        cards[i].lastElementChild.innerHTML = cardNumbers[i] > pairs ? cardNumbers[i] - pairs : cardNumbers[i]; 
+        number = cardNumbers[i];
+        cards[i].lastElementChild.innerHTML = number > pairs ? number - pairs : number; 
+        cards[i].lastElementChild.style.background = colorCode(pairs, number > pairs ? number - pairs : number);
     }
+
+    cards.forEach(function (elem) {
+        elem.addEventListener("click", function () {
+            if(!elem.classList.contains('is-flipped')) {
+                elem.classList.add('is-flipped');
+                if (counter == 0) {
+                    previouslyFlipped = elem;
+                    counter++;
+                } else {
+                    counter = 0;
+                    if(elem.innerHTML != previouslyFlipped.innerHTML) {
+                        setTimeout(function(){
+                            elem.classList.remove('is-flipped');
+                            previouslyFlipped.classList.remove('is-flipped');
+                        }, 750);                                            
+                    }
+                }
+                checkFlipped();
+            }
+        });    
+    });
 }
 
-function colorCode(code){
-
+function colorCode(pairs, number){
+    var code = Math.floor(765 / pairs * number);
+    return `rgb(${Math.floor(code/18)},${Math.floor(code/5)},${Math.floor(code/3)})`;
 }
 
 function shuffle(array) {
@@ -45,22 +70,30 @@ function shuffle(array) {
     return array;
 }
 
-let cards = document.querySelectorAll(".card");
-cards.forEach(function (elem) {
-    elem.addEventListener("click", function () {
-        elem.classList.add('is-flipped');
-        checkFlipped();
-    });    
-});
-
 function checkFlipped() {
     var r = true;
-    cards.forEach(function (elem) {
+    document.querySelectorAll(".card").forEach(function (elem) {
         if(!elem.classList.contains('is-flipped')) {
             r = false;
         }
     })
     if(r) {
         alert("All cards are flipped");
+    }
+}
+
+document.getElementById("board-generate").onclick = function() { 
+    Generate(); 
+}
+
+function Generate() {
+    var rowIndex = document.getElementById("board-rows").selectedIndex;
+    var rows = document.getElementById("board-rows").options;
+    var columnIndex = document.getElementById("board-columns").selectedIndex;
+    var columns = document.getElementById("board-columns").options;
+
+    console.log(rows[rowIndex].text + " " + columns[columnIndex].text)
+    if(!isNaN(rows[rowIndex].text) && !isNaN(columns[columnIndex].text) && ((rows[rowIndex].text * columns[columnIndex].text) % 2) == 0) {
+        generation(rows[rowIndex].text, columns[columnIndex].text);
     }
 }
