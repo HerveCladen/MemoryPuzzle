1656c5e10d24d6fec544ed77e088c146b3637c7f
Hervé Claden <hervecladen@gmail.com>
2019-04-03 15:45:04 -0400

    Random pairs
    Random pairs added
    No logic yet, no colors yet

--- scripts/script.js
+++ scripts/script.js
@@ -11,16 +11,44 @@ function generation(rowsInsert, columnsInsert) {
             row.innerHTML += '<div class="scene"><div class="card"><div class="card-face front"></div><div class="card-face back"></div></div></div>'
         }
     });
+
     let cards = document.querySelectorAll(".card");
-    cards.forEach(function(elem) {
-        elem.lastElementChild.style.background = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
-    });
+    var cardNumbers = [];
+    for(i = 0; i < cards.length; i++) {
+        cardNumbers.push(i+1);
+    } 
+
+    cardNumbers = shuffle(cardNumbers);
+    console.log(cards);
+    console.log(cardNumbers);
+
+    var pairs = cards.length / 2;
+    for(i = 0; i < cards.length; i++) {
+        cards[i].lastElementChild.innerHTML = cardNumbers[i] > pairs ? cardNumbers[i] - pairs : cardNumbers[i]; 
+    }
+}
+
+function colorCode(code){
+
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
 }
 
 let cards = document.querySelectorAll(".card");
 cards.forEach(function (elem) {
     elem.addEventListener("click", function () {
-        elem.classList.toggle('is-flipped');
+        elem.classList.add('is-flipped');
         checkFlipped();
     });    
 });
