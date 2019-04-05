4adc2abe970601e47da58a53d059f1b027be9bb0
Hervé Claden <hervecladen@gmail.com>
2019-04-04 21:27:38 -0400

    CSS and JS formatting

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-window.onload = generation(4,5);
+window.onload = generation(4, 5);
 
 var previouslyFlipped, cardFlipped;
 
@@ -7,56 +7,56 @@ function generation(rowsInsert, columnsInsert) {
 
     let board = document.getElementById("board");
     board.innerHTML = "";
-    for(i = 0; i < rowsInsert; i++) {
+    for (i = 0; i < rowsInsert; i++) {
         board.innerHTML += '<div class="row"></div>';
     }
 
     let rows = document.querySelectorAll(".row");
-    rows.forEach(function(row) {
-        for(i = 0; i < columnsInsert; i++) {
+    rows.forEach(function (row) {
+        for (i = 0; i < columnsInsert; i++) {
             row.innerHTML += '<div class="scene"><div class="card"><div class="card-face front"></div><div class="card-face back"></div></div></div>'
         }
     });
 
     let cards = document.querySelectorAll(".card");
     var cardNumbers = [];
-    for(i = 0; i < cards.length; i++) {
-        cardNumbers.push(i+1);
-    } 
+    for (i = 0; i < cards.length; i++) {
+        cardNumbers.push(i + 1);
+    }
     cardNumbers = shuffle(cardNumbers);
 
     var pairs = cards.length / 2, number;
-    for(i = 0; i < cards.length; i++) {
+    for (i = 0; i < cards.length; i++) {
         number = cardNumbers[i] > pairs ? cardNumbers[i] - pairs : cardNumbers[i];
-        cards[i].lastElementChild.innerHTML = number; 
+        cards[i].lastElementChild.innerHTML = number;
         cards[i].lastElementChild.style.background = colorCode(pairs, number);
     }
 
     cards.forEach(function (elem) {
         elem.addEventListener("click", function () {
-            if(!elem.classList.contains('is-flipped')) {
+            if (!elem.classList.contains('is-flipped')) {
                 elem.classList.add('is-flipped');
                 if (cardFlipped == false) {
                     previouslyFlipped = elem;
                     cardFlipped = true;
-                } else {                    
-                    if(elem.innerHTML != previouslyFlipped.innerHTML) {
-                        setTimeout(function(){
+                } else {
+                    if (elem.innerHTML != previouslyFlipped.innerHTML) {
+                        setTimeout(function () {
                             elem.classList.remove('is-flipped');
                             previouslyFlipped.classList.remove('is-flipped');
-                        }, 650);                                            
+                        }, 700);
                     }
                     cardFlipped = false;
                 }
                 checkFlipped();
             }
-        });    
+        });
     });
 }
 
-function colorCode(pairs, number){
+function colorCode(pairs, number) {
     var code = Math.floor(765 / pairs * number);
-    return `rgb(${Math.floor(code/18)},${Math.floor(code/5)},${Math.floor(code/3)})`;
+    return `rgb(${Math.floor(code / 18)},${Math.floor(code / 5)},${Math.floor(code / 3)})`;
 }
 
 function shuffle(array) {
@@ -74,13 +74,13 @@ function shuffle(array) {
 function checkFlipped() {
     var allFlipped = true;
     document.querySelectorAll(".card").forEach(function (elem) {
-        if(!elem.classList.contains('is-flipped')) {
+        if (!elem.classList.contains('is-flipped')) {
             allFlipped = false;
         }
     })
-    if(allFlipped) {
-        setTimeout(() => { 
-            alert("You found all pairs! Great job!");        
+    if (allFlipped) {
+        setTimeout(() => {
+            alert("You found all pairs! Great job!");
         }, 100);
     }
 }
@@ -91,7 +91,7 @@ function Generate() {
     var rows = document.getElementById("board-rows").options[document.getElementById("board-rows").selectedIndex].text;
     var columns = document.getElementById("board-columns").options[document.getElementById("board-columns").selectedIndex].text;
 
-    if(!isNaN(rows) && !isNaN(columns) && ((rows * columns) % 2) == 0) {
+    if (!isNaN(rows) && !isNaN(columns) && ((rows * columns) % 2) == 0) {
         generation(rows, columns);
     }
 }
